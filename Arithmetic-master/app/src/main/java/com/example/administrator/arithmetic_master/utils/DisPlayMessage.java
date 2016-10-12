d9165d4fd21cf0ce0f665116b0e12d2289d14057
github-lpw <iwen_migd@163.com>
2016-10-12 16:01:41 +0800

    login page
    Adding page

---
+++ Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/utils/DisPlayMessage.java
@@ -0,0 +1,14 @@
+package com.example.administrator.arithmetic_master.utils;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2016/10/11.
+ */
+public class DisPlayMessage {
+    public static void display(Context cxt) {
+        Toast.makeText(cxt, null, Toast.LENGTH_SHORT).show();
+    }
+}
--- Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/view/MainActivity.java
+++ Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/view/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.administrator.arithmetic_master.view;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.administrator.arithmetic_master.R;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -38,4 +43,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
